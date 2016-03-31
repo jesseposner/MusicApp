d931e28395ec6d81827c6cc7648bd70a5c31e6de
Jesse Posner <jesse.posner@gmail.com>
2016-03-31 11:28:20 -0700

    setup initial auth pattern

---
+++ app/models/user.rb
@@ -0,0 +1,50 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+
+class User < ActiveRecord::Base
+  validates :email, :password_digest, :session_token,
+            presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  def self.find_by_credentials(email, password)
+    user = User.find_by_email(email)
+    return user if user && user.is_password?(password)
+    nil
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def is_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+
+  private
+
+  attr_reader :password
+
+  def generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def reset_session_token
+    self.session_token = generate_session_token
+    save!
+  end
+
+  def ensure_session_token
+    self.session_token ||= generate_session_token
+  end
+end
